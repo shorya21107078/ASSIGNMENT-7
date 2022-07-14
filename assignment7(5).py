ce46dcd050aadca652a7eef07848ea2863462028
shorya21107078 <105016231+shorya21107078@users.noreply.github.com>
2022-07-14 23:22:17 +0530

    Update assignment7(5).py

--- assignment7(5).py
+++ assignment7(5).py
@@ -1,23 +1,76 @@
-def heapify(nums, heap_size, root_index):
-    
-    largest = root_index
-    left_child = (2 * root_index) + 1
-    right_child = (2 * root_index) + 2
-    if left_child < heap_size and nums[left_child] > nums[largest]:
-        largest = left_child
-    if right_child < heap_size and nums[right_child] > nums[largest]:
-        largest = right_child
-    if largest != root_index:
-        nums[root_index], nums[largest] = nums[largest], nums[root_index]
-        heapify(nums, heap_size, largest)
+print("Q5")
+#5a
 
-def heap_sort(nums):
-    n = len(nums)
-    for i in range(n, -1, -1):
-        heapify(nums, n, i)
-    for i in range(n - 1, 0, -1):
-        nums[i], nums[0] = nums[0], nums[i]
-        heapify(nums, i, 0)
-random_list_of_nums = [35, 12, 43, 8, 51]
-heap_sort(random_list_of_nums)
-print(random_list_of_nums)
+def partition(l, r, nums):
+    # Last element will be the pivot and the first element the pointer
+    pivot, ptr = nums[r], l
+    for i in range(l, r):
+        if nums[i] <= pivot:
+            # Swapping values smaller than the pivot to the front
+            nums[i], nums[ptr] = nums[ptr], nums[i]
+            ptr += 1
+    # Finally swapping the last element with the pointer indexed number
+    nums[ptr], nums[r] = nums[r], nums[ptr]
+    return ptr
+ 
+# With quicksort() function, we will be utilizing the above code to obtain the pointer
+# at which the left values are all smaller than the number at pointer index and vice versa
+# for the right values.
+ 
+ 
+def quicksort(l, r, nums):
+    if len(nums) == 1:  # Terminating Condition for recursion. VERY IMPORTANT!
+        return nums
+    if l < r:
+        pi = partition(l, r, nums)
+        quicksort(l, pi-1, nums)  # Recursively sorting the left values
+        quicksort(pi+1, r, nums)  # Recursively sorting the right values
+    return nums
+
+n = eval(input("Enter a list in [] brackets:"))
+
+print(quicksort(0,len(n)-1,n))
+
+#5b
+def binary_search(list1, x):  
+    low = 0  
+    high = len(list1) - 1  
+    mid = 0  
+  
+    while low <= high:  
+        # for get integer result   
+        mid = (high + low) // 2  
+  
+        # Check if n is present at mid   
+        if list1[mid] < x:  
+            low = mid + 1  
+  
+        # If n is greater, compare to the right of mid   
+        elif list1[mid] > x:  
+            high = mid - 1  
+  
+        # If n is smaller, compared to the left of mid  
+        else:  
+            return mid  
+  
+            # element was not present in the list, return -1  
+    return -1  
+  
+  
+# Initial list1    
+x = int(input("Enter the element you want to search :"))
+  
+# Function call   
+result = binary_search(n, x)  
+  
+if result != -1:  
+    print("Element is present at index", n.index(x))  
+else:  
+    print("Element is not present in list1")  
+
+#5c
+count = 0
+for i in n:
+    if i == x:
+        count += 1
+print(f'Number of occurences of element {x} is: {count}')
